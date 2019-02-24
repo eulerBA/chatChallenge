eb8d3a9d0f6c2561bae7689f692ee6f80d9c9606
Leandro Prior <lprior@medallia.com>
2019-02-24 04:13:10 -0300

    exercise 4

--- src/Test/BaseTest.java
+++ src/Test/BaseTest.java
@@ -1,5 +1,8 @@
 package Test;
 
+import Pages.ChatRoomPage;
+import Pages.HomePage;
+import Pages.LoginPage;
 import Pages.Page;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -21,8 +24,7 @@ public class BaseTest {
     public static String baseUrl = "https://chatchallengeproj.herokuapp.com/";
     public final String r = getRandomNumber();
 
-
-    public void setup(int users){
+    public void setupTest(int users){
         WebDriver driver;
         Page p;
         for(int i=1; i<=users; i++) {
@@ -57,4 +59,51 @@ public class BaseTest {
         }
     }
 
+    protected Map<Integer, ChatRoomPage> enterToRoomAllUsers(Map<Integer, HomePage> homePages, int rooms) {
+        Map<Integer, ChatRoomPage> chatRoomPages = new HashMap<Integer, ChatRoomPage>();
+        ChatRoomPage crp;
+        ArrayList<String> listUsernames;
+        int random = 0;
+        for(int i=1; i<=homePages.size(); i++) {
+            int index = (int) (Math.random() * (rooms - 1)) + 1;
+            crp = homePages.get(i).enterToRoom("Room "+index+ " " +r);
+            chatRoomPages.put(i,crp);
+            listUsernames = crp.listUsernames();
+            for(int x=0; x<listUsernames.size(); x++)
+                if (!("User " + i + " " + r).equals(listUsernames.get(x))){
+                    crp.sendMessage("HELLO " + listUsernames.get(x));
+                }
+
+        }
+        return chatRoomPages;
+    }
+
+    protected void createAllRooms(HomePage homePage, int rooms) {
+        for (int i = 1; i <= rooms; i++) {
+            homePage.createNewRoom("Room "+i+ " " +r);
+        }
+    }
+
+    protected Map<Integer,HomePage> loginAllUsers(Map<Integer, LoginPage> loginPages) {
+        Map<Integer, HomePage> homePages = new HashMap<Integer, HomePage>();
+        LoginPage lp;
+        HomePage hp;
+        for (int i = 1; i <= loginPages.size(); i++) {
+            lp = loginPages.get(i);
+            hp = lp.login("User " + i + " " + r,"pass123");
+            homePages.put(i,hp);
+        }
+        return homePages;
+    }
+
+    protected Map<Integer,LoginPage> createAllUsers() {
+        Map<Integer, LoginPage> loginPages = new HashMap<Integer, LoginPage>();
+        LoginPage lp;
+        for (int i = 1; i <= drivers.size(); i++) {
+            lp = new LoginPage(drivers.get(i));
+            lp.createNewUser("User " + i + " " + r, "pass123");
+            loginPages.put(i, lp);
+        }
+        return loginPages;
+    }
 }
